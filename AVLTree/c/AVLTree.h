9fba0717dce04c80a0bce646f8f2c173dbecb387
yuhaiming <yuhaiming@turingcat.com>
2021-09-02 15:17:50 +0800

    feat:add avltree delete node

--- AVLTree/c/AVLTree.h
+++ AVLTree/c/AVLTree.h
@@ -32,6 +32,9 @@ AVLTree rotateRightLeft(AVLTree node);
 // 左右旋转（LR旋转）
 AVLTree rotateLeftRight(AVLTree node);
 
+// 再平衡 通过旋转让AVL 树再度平衡
+AVLTree rebalance(const int data, AVLTree node);
+
 // 节点 struct
 struct AVLNode
 {
@@ -75,6 +78,27 @@ static int getMaxHeight(AVLTree node)
     return maxNumber(height(node->left), height(node->right)) + 1;
 }
 
+// 递归查找左节点 为最小
+AVLTree findMin(AVLTree tree)
+{
+    if (tree == NULL)
+        return NULL;
+    if (tree->left == NULL)
+        return tree;
+    return findMin(tree->left);
+}
+// 循环查找右节点 为最大
+AVLTree findMax(AVLTree tree)
+{
+    if (tree != NULL)
+    {
+        while (tree->right != NULL)
+        {
+            tree = tree->right;
+        }
+    }
+    return tree;
+}
 // 左旋转（LL旋转）
 AVLTree rotateLeft(AVLTree node)
 {
@@ -127,7 +151,42 @@ AVLTree rotateRightLeft(AVLTree node)
     // 再对当前旋转节点进行左旋转
     return rotateLeft(node);
 }
+// 再平衡 通过旋转让AVL 树再度平衡
+AVLTree rebalance(const int data, AVLTree node)
+{
+    if (node == NULL)
+        return NULL;
+    // 两颗子树的高度差不大于1，则该节点是平衡的
 
+    // 左子树的高度大于右子树的高度 并且差值大于1 节点不平衡
+    if (height(node->left) - height(node->right) > 1)
+    {
+        // 当前插入的值小于 左子树的值 说明 这个新节点插入到了左子树的左子树节点的位置 进行右旋转
+        if (data < node->left->data)
+        {
+
+            node = rotateRight(node);
+        }
+        else // 否则就是插入了 左子树的右子树节点位置 进行左右旋转
+        {
+            node = rotateLeftRight(node);
+        }
+    }
+    // 右子树的高度大于左子树的高度 并且差值大于1 节点不平衡
+    else if (height(node->right) - height(node->left) > 1)
+    {
+        // 当前插入的值大于 右子树的值 说明 这个新节点插入到了 右子树的右子树的位置 进行左旋转
+        if (data > node->right->data)
+        {
+            node = rotateLeft(node);
+        }
+        else // 否则就是在右子树的左子树位置 进行右左旋转
+        {
+            node = rotateRightLeft(node);
+        }
+    }
+    return node;
+};
 AVLTree insert(const int data, AVLTree tree)
 {
     if (tree == NULL)
@@ -137,49 +196,65 @@ AVLTree insert(const int data, AVLTree tree)
     else if (data < tree->data)
     { //比当前节点小 插入左子树中
         tree->left = insert(data, tree->left);
-        // 两颗子树的高度差不大于1，则该节点是平衡的
-        if (height(tree->left) - height(tree->right) > 1)
-        {
-              // 当前插入的值小于 左子树的值 说明 这个新节点插入到了左子树的左子树节点的位置 进行右旋转
-            if (data < tree->left->data)
-            {
-
-                tree = rotateRight(tree);
-            }
-            else // 否则就是插入了 左子树的右子树节点位置 进行左右旋转
-            {
-                tree = rotateLeftRight(tree);
-            }
-        }
     }
     else if (data > tree->data)
     {
         // 比当前节点的值大，插入右子树中
         tree->right = insert(data, tree->right);
-        // 两颗子树的高度差不大于1，则该节点是平衡的
-        if (height(tree->right) - height(tree->left) > 1)
-        {
-            // 当前插入的值大于 右子树的值 说明 这个新节点插入到了 右子树的右子树的位置 进行左旋转
-            if (data > tree->right->data)
-            {
-                tree = rotateLeft(tree);
-            }
-            else // 否则就是在右子树的左子树位置 进行右左旋转
-            {
-                tree = rotateRightLeft(tree);
-            }
-        }
     }
+    // 再平衡
+    tree = rebalance(data, tree);
     tree->height = getMaxHeight(tree);
     return tree;
 }
+
+AVLTree deleteNode(const int data, AVLTree tree)
+{
+    if (tree == NULL)
+        return NULL;
+
+    if (tree->data > data)
+    {
+        // 小于当前节点值 去左子树节点查找
+        tree->left = deleteNode(data, tree->left);
+    }
+    else if (tree->data < data)
+    {
+        // 大于当前节点值 右去子树节点查找
+        tree->right = deleteNode(data, tree->right);
+    }
+    else if (tree->left && tree->right)
+    {
+        // 找到右子树最小的节点，替换当前节点
+        AVLTree temp = findMin(tree->right);
+        tree->data = temp->data;
+        // 再将最小节点删除
+        tree->right = deleteNode(temp->data, tree->right);
+    }
+    else
+    {
+        AVLTree temp = tree;
+        if (tree->left == NULL)
+        {
+            tree = tree->right;
+        }
+        else if (tree->right == NULL)
+        {
+            tree = tree->left;
+        }
+        free(temp);
+    }
+    // 再平衡
+    tree = rebalance(data, tree);
+    return tree;
+};
 
 void printTree(AVLTree tree, int level)
 {
     if (tree != NULL)
     {
-        printf("node data = %d, height = %d , level = %d\n", tree->data, tree->height, level);
         printTree(tree->left, level + 1);
+        printf("node data = %d, height = %d , level = %d\n", tree->data, tree->height, level);
         printTree(tree->right, level + 1);
     }
 }
--- AVLTree/c/main.c
+++ AVLTree/c/main.c
@@ -6,14 +6,21 @@ int main(int argc, char const *argv[])
 {
     /* code */
     AVLTree tree = NULL;
-    int arr[] = {70, 80, 90, 20, 10, 50, 60, 40, 30};
+    int size = 16;
+    int arr[16] = {70, 80, 90, 20, 10, 50, 60, 40, 30, 1, 8, 47, 95, 150, 635, 101};
 
-    for (size_t i = 0; i < 9; i++)
+    for (size_t i = 0; i < size; i++)
     {
         /* code */
         tree = insert(arr[i], tree);
     }
 
+    printTree(tree, 0);
+    puts("0000000000000000000000000000000000");
+    tree = deleteNode(70, tree);
+    puts("------------------------------delete");
+    tree = deleteNode(80, tree);
+    puts("------------------------------delete");
     printTree(tree, 0);
     return 0;
 }
